1e8f86a931a7964021a68bf0b4653d24bb8bc516
AkAttack <akiempaul@gmail.com>
2022-07-20 16:23:01 -0400

    Last Work

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,11 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
   return ( 
     <header className="container">
+      <span>
+        <img src="process.env.PUBLIC_URL/company-logo.png" alt="company Logo" />
+        <h4>E-Mag Online</h4>
+      </span>
+
       <ul>
         <li><Link to= "/">Home</Link></li>
         <li><Link to= "/quotes">Quotes</Link></li>
