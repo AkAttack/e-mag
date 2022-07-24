2cd8f19dfec7746b762d9213eefda50f98424c83
AkAttack <akiempaul@gmail.com>
2022-07-24 00:06:36 -0400

    home last

--- frontend/src/components/OrderItemForm.js
+++ frontend/src/components/OrderItemForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const OrderItemForm = ({itemNum, removeItem, activeItem, hideWindow}) => {
+const OrderItemForm = ({itemNum, cartInfo, addItem, toggleExpand, keyId, updateCartInfo, activeItems}) => {
 
   const [description, setDescription] = useState("")
   const [url, setUrl] = useState("")
@@ -11,108 +11,104 @@ const OrderItemForm = ({itemNum, removeItem, activeItem, hideWindow}) => {
   const [weightLB, setWeightLB] = useState("")
   const [weightPrice, setWeightPrice] = useState("0") // needs setup
   const [error, setError] = useState(null)
-  const [collaspe, setCollaspe] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
 
-    const workout = { description, url, color, size, itemCategory, itemPrice, weightLB, weightPrice };
+  //   const workout = { description, url, color, size, itemCategory, itemPrice, weightLB, weightPrice };
 
-    const response = await fetch('http://localhost:3001/api/orderitems/', {
-      method: 'POST',
-      body: JSON.stringify(workout),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const json = await response.json()
+  //   const response = await fetch('http://localhost:3001/api/orderitems/', {
+  //     method: 'POST',
+  //     body: JSON.stringify(workout),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   const json = await response.json()
 
-    if (!response.ok) {
-      setError(json.error)
-    }
-    if (response.ok) {
-      setError(null)    
-      console.log('new Cart Item added:', json)
-    }
+  //   if (!response.ok) {
+  //     setError(json.error)
+  //   }
+  //   if (response.ok) {
+  //     setError(null)    
+  //     console.log('new Cart Item added:', json)
+  //   }
   
-  }
+  // }
 
-  const collaspeWin = () => {
-    setCollaspe((prevState) => {
-      return !prevState
-    })
-  }
 
-  return (
-    <div className="OIF">
+  return (cartInfo[keyId].active &&
+    <div className="OIF" >
       <div className="title">
-        <span className="OIF-+" onClick={() => collaspeWin(itemNum)}>{collaspe ? "+" : "-"}</span> Item {itemNum}  
-        {(activeItem === itemNum && !(activeItem === 1)) && 
-        <span className="itemDelet-button" onClick={() => removeItem(itemNum)}>      DEL</span>}</div>
-      {(collaspe | itemNum === 1) &&
-      <div className="content">
-        <form onSubmit={handleSubmit}>
-          <div className="item-details">
-            <div className="input-box">
-              <input type="text" 
-                name="description" 
-                value={description} 
-                placeholder="Item Description" 
-                onChange={(e) => setDescription(e.target.value)} />
+        <span className="OIF-+" onClick={() => {toggleExpand(keyId)}}>{cartInfo[keyId].expandShow ? "+" : "-"}</span> Item {itemNum}  
+        {(activeItems === itemNum) && (itemNum !== 1) && <span className="itemDelet-button" onClick={updateCartInfo}>      DEL</span>} 
+      </div>
+
+      {cartInfo[keyId].expandShow &&
+        <div className="content">
+          <div className="form-item">
+            <div className="item-details">
+              <div className="input-box">
+                <input type="text" 
+                  name="description" 
+                  value={description} 
+                  placeholder="Item Description" 
+                  onChange={(e) => setDescription(e.target.value)} />
+              </div>
+              <div className="input-box">
+                <input type="text" 
+                  name="url" 
+                  placeholder="Item Link(URL)" 
+                  required 
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}/>
+              </div>
+              <div className="input-box">
+                <input type="text" 
+                  name="color" 
+                  placeholder="Color"
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)} />
+              </div>
+              <div className="input-box">
+                <input type="text" 
+                  name="size" 
+                  placeholder="Size"
+                  value={size}
+                  onChange={(e) => setSize(e.target.value)} />
+              </div>
+              <div className="input-box">
+                <input type="text" 
+                  name="itemCategory" 
+                  placeholder="Category(eg. laptop, clothes, tv, jewelery, car parts)" 
+                  required
+                  value={itemCategory}
+                  onChange={(e) => setItemCategory(e.target.value)} />
+              </div>
+              <div className="input-box">
+                <input type="number" 
+                  name="itemPrice" 
+                  placeholder="Item Price" 
+                  required
+                  value={itemPrice}
+                  onChange={(e) => setItemPrice(e.target.value)} />
+              </div>
+              <div className="input-box">
+                <input type="number" 
+                  name="weightLB" 
+                  placeholder="Item Weight (LB)" 
+                  required
+                  value={weightLB}
+                  onChange={(e) => setWeightLB(e.target.value)} />
+              </div>
             </div>
-            <div className="input-box">
-              <input type="text" 
-                name="url" 
-                placeholder="Item Link(URL)" 
-                required 
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}/>
-            </div>
-            <div className="input-box">
-              <input type="text" 
-                name="color" 
-                placeholder="Color"
-                value={color}
-                onChange={(e) => setColor(e.target.value)} />
-            </div>
-            <div className="input-box">
-              <input type="text" 
-                name="size" 
-                placeholder="Size"
-                value={size}
-                onChange={(e) => setSize(e.target.value)} />
-            </div>
-            <div className="input-box">
-              <input type="text" 
-                name="itemCategory" 
-                placeholder="Category(eg. laptop, clothes, tv, jewelery, car parts)" 
-                required
-                value={itemCategory}
-                onChange={(e) => setItemCategory(e.target.value)} />
-            </div>
-            <div className="input-box">
-              <input type="number" 
-                name="itemPrice" 
-                placeholder="Item Price" 
-                required
-                value={itemPrice}
-                onChange={(e) => setItemPrice(e.target.value)} />
-            </div>
-            <div className="input-box">
-              <input type="number" 
-                name="weightLB" 
-                placeholder="Item Weight (LB)" 
-                required
-                value={weightLB}
-                onChange={(e) => setWeightLB(e.target.value)} />
+            <div className="button">
+              <button onClick={addItem}>Add Item</button>
             </div>
           </div>
-          <div className="button">
-            <button type="submit">Generate</button>
-          </div>
-        </form>
-      </div> }
-    </div>
+        </div>
+      }
+    </div> 
    );
 }
  
--- frontend/src/pages/CreateQuote.js
+++ frontend/src/pages/CreateQuote.js
@@ -3,38 +3,86 @@ import OrderItemForm from "../components/OrderItemForm";
 // import {v4 as uuid} from "uuid";
 
 const CreateQuote = () => {
+  const MAX_CART_AMOUNT = 8
+  const MIN_CART_AMOUNT = 1
 
-  const [cartAmount, setCartAmount] = useState([{id: 1, active: true}, {id: 2, active:false},{id: 3, active:false},{id: 4, active:false},{id: 5, active:false},{id: 6, active:false},{id: 7, active:false},{id: 8, active:false}])
-  let activeItems = 1
-
+  const [cartInfo, setCartInfo] = useState([{id: 1, active: true, expandShow: true}, {id: 2, active:false, expandShow: false},{id: 3, active:false, expandShow: false},{id: 4, active:false, expandShow: false},{id: 5, active:false, expandShow: false},{id: 6, active:false, expandShow: false},{id: 7, active:false, expandShow: false},{id: 8, active:false, expandShow: false}])
+  let [activeItems, setActiveItems] = useState(MIN_CART_AMOUNT)
   
-  const removeItem = (id) => {
-    setCartAmount(prevState => {
-      const newArr = [...prevState]
-      newArr[id - 1] = !newArr[id - 1].active
-      return newArr
-    })
+  const removeItem = () =>{
+    if(activeItems >= 2) {
+      const id = activeItems - 1
+      const newArr = [...cartInfo]
+        newArr[id].active = false
+        newArr[id].expandShow = true
+        if(id >= 1){newArr[id - 1].expandShow = true}
+      if(id < MAX_CART_AMOUNT && id >= MIN_CART_AMOUNT){
+        setCartInfo(newArr)
+      }
+      const newNum = activeItems - 1
+      if(newNum >= MIN_CART_AMOUNT && newNum <= MAX_CART_AMOUNT){ 
+        setActiveItems(newNum) }    
+    }
   }
 
   const addItem = () => {
-    setCartAmount(prevState => {
-      const newArr = [...prevState]
-      newArr[activeItems - 1] = !newArr[activeItems - 1].active
-      activeItems += 1
-      return newArr
-    })
+    const id = activeItems 
+    if(id < MAX_CART_AMOUNT && id >= MIN_CART_AMOUNT){
+      const newArr = [...cartInfo]
+      newArr[id].active = true
+      newArr[id].expandShow = true
+      newArr[id - 1].expandShow = false
+      setCartInfo(newArr)
+    }
+    const newNum = activeItems + 1
+    if(newNum >= MIN_CART_AMOUNT && newNum <= MAX_CART_AMOUNT){ 
+      setActiveItems(newNum) }    
   }
 
+  const toggleExpand = (id) => {
+    const newArr = [...cartInfo]
+    newArr[id].expandShow = !cartInfo[id].expandShow
+    setCartInfo(newArr)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await fetch('http://localhost:3001/api/orderitems/', {
+      method: 'POST',
+      body: JSON.stringify({test: "wwwww"}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      console.log("ERROR Encountered")
+    }
+    if (response.ok) {
+      console.log('new Cart Item added:', json)
+    }
+  
+  }
 
   return ( 
-    <div className="create-quote">
-      {cartAmount.map((e,i) => {
-        return (e.active && <OrderItemForm key={e.id} itemNum={e.id} activeItems={activeItems}  active={e.active} removeItem={removeItem}/>)
+    <form className="create-quote" onSubmit={handleSubmit}>
+      {cartInfo.map((e,i) => {
+        return (<OrderItemForm 
+          key={i} 
+          keyId={i}
+          itemNum={e.id} 
+          updateCartInfo={removeItem}
+          activeItems={activeItems}
+          toggleExpand={toggleExpand}
+          addItem={addItem}
+          cartInfo={cartInfo}/>)
       })  }
-      <button onClick={() => addItem(activeItems + 1)}>ADD ITEM</button>
-      <button onClick={() => console.log(cartAmount)}>Cart Item Amount</button>
-      <button onClick={() => removeItem(activeItems - 1)}>Remove Last Item</button>
-    </div>
+      <button onClick={addItem}>ADD ITEM</button>
+      <button onClick={() => console.log(activeItems)}>Cart Item Amount</button>
+      <button onClick={removeItem}>Remove Last Item</button>
+    </form>
    );
 
 }
