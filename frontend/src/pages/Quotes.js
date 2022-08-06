9d7b31ac90ddbffd1f79a99bd8f6fd3c3442fd40
AkAttack <akiempaul@gmail.com>
2022-08-06 15:53:58 -0400

    home LAST

--- frontend/src/pages/Quotes.js
+++ frontend/src/pages/Quotes.js
@@ -1,9 +1,12 @@
 import {QUOTE_INFO} from "../GlobalVars"
 import { saveAs } from 'file-saver';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import QuoteTemplate from "../components/QuoteTemplate";
 
 const Quote = () => {
   const [allQuotes, setAllQuotes] = useState({loaded: false, quotes: []})
+  const [quotePreview, setQuotePreview] = useState({display: false, quote: {}})
+
 
   const fetchErrorCheck = (res) => {
     if(res.status >= 200 && res.status <= 299){
@@ -28,19 +31,47 @@ const Quote = () => {
   }
 
   const getAllQuotes = () => {
-    fetch("/api/quotes")
+    fetch("/api/quotations")
       .then(fetchErrorCheck)
-      .then(() => {
-        
+      .then((json) => {
+        const newAllQuote = {...allQuotes}
+        console.log(json)
+        newAllQuote.quotes = json
+        newAllQuote.loaded = true
+        setAllQuotes(newAllQuote)
       })  
       .catch(err => {console.log(err)})
   }
+  const showQuotePreview = (quote) =>{
+    const newQuotePreview = {...quotePreview}
+    newQuotePreview.quote = quote
+    newQuotePreview.display = true
+    setQuotePreview(newQuotePreview)
+  }
+
+  useEffect(() => {
+    getAllQuotes()
+  }, [])
 
 
   return ( 
     <div>
-      <button onClick={createAndDownloadPdf}>Download PDF Test</button>
-      <button onClick={() => console.log(QUOTE_INFO)}>QUOTE_INFO</button>
+
+      {!quotePreview.display && allQuotes.quotes.map((item,i) =>{
+        return (
+        <div className="quote-item" quote={item} onClick={()=>{showQuotePreview(item)}}>
+          <p>{item.quoteID}  || Created: {item.createdAt.slice(0,10)}</p>
+          <p>Customer: {item.customer.namefirst} {item.customer.namelast}</p>
+          <p>Total: {item.target.grandTotal.toLocaleString("en-US", {style:"currency", currency:"USD"})} || No. Items {[...item.cart.map(item=>{ if(item.active){return item} })].length} </p>
+        </div>)
+      })
+      }
+      {quotePreview.display && 
+        <div>
+          <button onClick={()=>{setQuotePreview({display: false, quote: {}})}}>Close Preview</button>
+          <QuoteTemplate quoteInfo={quotePreview.quote}/>
+        </div>
+      }
     </div>
    );
 }
